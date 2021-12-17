49020bca05313d5f6f7e890e598a26a7e76ef69d
Acutapugione <88377885+Acutapugione@users.noreply.github.com>
2021-12-17 11:01:31 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ python {file_path}\main.py
 from FinanceManager import *
 
 try:
-    my_fm = FinanceManager('test.txt')
+    my_fm = FinanceManager('{your_db_file}.txt')
     my_fm.initUserMenu()
 except ValueError as e:
     print(e)
